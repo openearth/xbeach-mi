29f0769e2690f89f2695e71d64fa8079c37064e5
Willem Vermin <wvermin@gmail.com>
2016-08-21 15:46:37 +0200

    willem

--- mkmap/interpol.py
+++ mkmap/interpol.py
@@ -1,20 +1,20 @@
 import sys
 import numpy as np
+try:
+    from pykdtree.kdtree import KDTree
+    print 'Using pykdtree'
+except:
+    from scipy.spatial import cKDTree as KDTree
+    print 'Using scipy.spatial'
+# pykdtree from https://github.com/storpipfugl/pykdtree is much faster:
+#    from pykdtree.kdtree import KDTree
 class interpolkd:
 #
 # works with scipy and pykdtree:
 #
-    try:
-        from pykdtree.kdtree import KDTree
-        print 'Using pykdtree'
-    except:
-        from scipy.spatial import cKDTree as KDTree
-        print 'Using scipy.spatial'
-# pykdtree from https://github.com/storpipfugl/pykdtree is much faster:
-#    from pykdtree.kdtree import KDTree
 
     def __init__(self, X, z=None, max_dist=-1, fill_value=np.NaN, leafsize=10):
-        self.tree = self.KDTree(X, leafsize=leafsize)
+        self.tree = KDTree(X, leafsize=leafsize)
         self.length = len(X)
         self.setfill_value(fill_value)
         if z is not None:
@@ -54,12 +54,13 @@ class interpolkd:
 
         return wz if qdim > 1 else wz[0]
         
+from scipy.spatial import Delaunay
+from scipy.interpolate import LinearNDInterpolator
 class interpoldln:
-    from scipy.spatial import Delaunay
-    from scipy.interpolate import LinearNDInterpolator
     def __init__(self,X,z=None,fill_value=np.NaN):
-        self.tri = self.Delaunay(X)
-        self.length = len(X)
+        self.tri = Delaunay(X)
+        self.length = X.shape[0]
+        self.dim = X.shape[1]
         self.setfill_value(fill_value)
         if z is not None:
             self.setz(z)
@@ -67,7 +68,9 @@ class interpoldln:
     def setz(self,z):
         assert self.length == len(z), "len(X) %d != len(z) %d" % (self.length, len(z))
         self.z = z.copy()
-        self.F = self.LinearNDInterpolator(self.tri, self.z, fill_value=self.fill_value)
+        self.F = LinearNDInterpolator(self.tri, self.z, fill_value=self.fill_value)
+        # warming up this function:
+        self.F(np.zeros(self.dim))
 
     def setfill_value(self,fill_value):
         self.fill_value = fill_value
@@ -77,4 +80,71 @@ class interpoldln:
         return result
 
 if __name__ == "__main__":
-    pass
+    def tic():
+        # Homemade version of matlab tic and toc functions
+        import time
+        global startTime_for_tictoc
+        startTime_for_tictoc = time.time()
+
+
+    def toc():
+        import time
+        if 'startTime_for_tictoc' in globals():
+            print "Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds."
+        else:
+            print "Toc: start time not set"
+
+    r=np.random.random
+    n=300
+    x = np.linspace(-1,1,n+1)
+    y = np.linspace(-2,2,n+1)
+    xv,yv = np.meshgrid(x,y)
+    X = np.array((xv.flatten(),yv.flatten())).T
+    z = np.array(r(X.shape[0]))
+    z = np.sum(np.sin(X),1)
+    q = np.array((r(5),r(5))).T-0.5
+    print 'number of grid points:',X.shape[0]
+    print 'computing kd tree:'
+    tic()
+    tree = interpolkd(X,z=z,fill_value=-123.)
+    toc()
+    print 'computing dln tree:'
+    tic()
+    tri = interpoldln(X,z,fill_value=-123.)
+    toc()
+    rtree = tree(q)
+    rtri = tri(q)
+
+    print 'kd ',rtree
+    print 'dln',rtri
+
+    print 
+    m = 100000
+    print 'interpolating',m,'random points'
+    qq = np.array((r(m),r(m))).T-0.5
+    print 'kd tree:'
+    tic()
+    rtree = tree(qq)
+    toc()
+    qq = np.array((r(m),r(m))).T-0.5
+    print 'dln tree:',qq.shape
+    tic()
+    rtri = tri(qq)
+    toc()
+
+    nx = int(np.sqrt(m))
+    ny = nx
+    xx = np.linspace(-0.9,0.9,nx+1)
+    yy = np.linspace(-1.9,1.9,ny+1)
+    xxv,yyv = np.meshgrid(xx,yy)
+    qqq = np.array((xxv.flatten(),yyv.flatten())).T
+    print 'interpolating',qqq.shape[0],'regular grid points'
+
+    print 'kd tree:'
+    tic()
+    rtree = tree(qqq)
+    toc()
+    print 'dln tree:'
+    tic()
+    rtri = tri(qqq)
+    toc()
--- mkmap/mkmaptrikd.py
+++ mkmap/mkmaptrikd.py
@@ -8,19 +8,18 @@ Created on Tue Aug  2 15:24:17 CEST 2016
 using kdtree
 """
 import sys
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from interpol import interpoldln,interpolkd
 
 def tic():
     # Homemade version of matlab tic and toc functions
-    import time
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
 
 
 def toc():
-    import time
     if 'startTime_for_tictoc' in globals():
         print "Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds."
     else:
@@ -86,36 +85,41 @@ for j in range(n2):
 print 'grid 1 ', m1 * n1, ' points, grid 2 ', m2 * n2, ' points'
 toc()
 gr = np.asarray((x1.flatten(), y1.flatten())).T
-tic()
 print 'testing Delaunay'
+tic()
 tri = interpoldln(gr,z1.flatten(),fill_value=amp)
 toc()
-tic()
 print 'testing kdtree'
 distx = abs(gr[0,0]-gr[1,0])
 disty = abs(gr[0,0]-gr[1,1])
 max_dist = 1.1*np.sqrt(distx**2 + disty**2)
 max_dist = max(distx,disty)
+tic()
 tree = interpolkd(gr,z1.flatten(),fill_value=amp,max_dist=max_dist)
 toc()
-tic()
+
+q = np.asarray((x2.flatten(), y2.flatten())).T
 print 'testing Delaunay interpolation'
-z2 = tri(np.asarray((x2.flatten(), y2.flatten())).T)
+tic()
+z2 = tri(q)
 toc()
+
 if Plot:
-    tic()
     print 'plotting results for Delaunay interpolation:'
+    tic()
     plotit('000dln.png',x1,y1,z1,x2,y2,z2,amp)
     toc()
-tic()
+
 print 'testing kdtree interpolation'
-z2 = tree(np.asarray((x2.flatten(), y2.flatten())).T)
+tic()
+z2 = tree(q)
 toc()
 if Plot:
-    tic()
     print 'plotting results for kdtree interpolation:'
+    tic()
     plotit('000kd.png',x1,y1,z1,x2,y2,z2,amp)
     toc()
+
 for y0 in range(500,5000,500):
     print 'shift hump in grid 1 and interpolate again'
     z1 = amp * np.exp(-((x1 - x0) ** 2 + (y1 - y0) ** 2) / L ** 2)
@@ -123,20 +127,19 @@ for y0 in range(500,5000,500):
     tree.setz(z1.flatten())
     print 'Delaunay:'
     tic()
-    z2 = tri(np.asarray((x2.flatten(), y2.flatten())).T)
+    z2 = tri(q)
     toc()
     if Plot:
-        tic()
         print 'plotting Delaunay'
+        tic()
         plotit('%ddln.png'%y0,x1,y1,z1,x2,y2,z2,amp)
         toc()
     print 'kdtree:'
     tic()
-    z2 = tree(np.asarray((x2.flatten(), y2.flatten())).T)
+    z2 = tree(q)
     toc()
     if Plot:
-        tic()
         print 'plotting kdtree'
+        tic()
         plotit('%dkd.png'%y0,x1,y1,z1,x2,y2,z2,amp)
         toc()
-    toc()
